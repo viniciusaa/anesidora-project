43e5f04cdc001475868ec5cf5e6947d393b89e51
“Vinícius <vini89go@gmail.com>
2020-09-25 00:27:08 -0300

    Added contributor update article body spec

---
+++ spec/features/article_bodies/contributor_update_article_body_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.feature "Contributor update article", :type => :feature do
+  before do
+    @user = create(:user)
+    @second_user = create(:user)
+    @article = create(:article, user_id: @user.id)
+    @article_body = create(:article_body, article_id: @article.id)
+    login_as(@second_user)
+  end
+
+  scenario "When is a contributor" do
+    @article.contributors << @second_user
+    visit "/"
+    click_link "Articles"
+    click_link @article.name
+    click_link "Create new version"
+    fill_in "Body", with: "Test New Article version"
+    click_button "Confirm"
+
+    expect(page).to have_content("Created new article body version")
+    expect(page).to have_content("Test New Article version")
+  end
+
+  scenario "When not a contributor" do
+    visit "/"
+    click_link "Articles"
+    click_link @article.name
+    expect(page).to_not have_link("Create new version")
+  end
+end
