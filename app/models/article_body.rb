e2e1fb18705732e1588de549d83c25d6167c4f19
“Vinícius <vini89go@gmail.com>
2020-10-23 00:46:59 -0300

    Added actiontext to articlebody

--- app/models/article_body.rb
+++ app/models/article_body.rb
@@ -2,6 +2,8 @@ class ArticleBody < ApplicationRecord
   validates :body, presence: true, length: { minimum: 4, maximum: 10000 }
   validates :article_id, presence: true
 
+  has_rich_text :body
+
   default_scope -> { order(created_at: :desc) }
 
   belongs_to :article
